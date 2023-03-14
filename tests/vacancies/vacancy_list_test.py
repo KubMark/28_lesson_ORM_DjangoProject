844d3d9c315b95725611bd3dc64540fdf1360e91
macbook <mypur86@yahoo.com>
2023-03-14 22:39:18 +0300

    batch

--- tests/vacancies/vacancy_list_test.py
+++ tests/vacancies/vacancy_list_test.py
@@ -2,25 +2,30 @@ from datetime import date
 
 import pytest
 
-from vacancies.models import Vacancy
+from tests.factories import VacancyFactory
+from vacancies.serializers import VacancyListSerializer
 
 
 @pytest.mark.django_db
-def test_vacancy_list(client, vacancy):
+def test_vacancy_list(client):
+    vacancies = VacancyFactory.create_batch(10)
+
     expected_response = {
-        "count": 1,
+        "count": 10,
         "next": None,
         "previous": None,
+        "results": VacancyListSerializer(vacancies, many=True).data
+
         #  всё что возвращает VacancyListSerializer
-        "results": [{
-            "id": vacancy.pk,
-            "text": "test text",
-            "slug": "test",
-            "status": "draft",
-            "created": date.today().strftime("%Y-%m-%d"),
-            "username": "test",
-            "skills": []
-        }]
+        # "results": [{
+        #     "id": vacancy.pk,
+        #     "text": "test text",
+        #     "slug": "test",
+        #     "status": "draft",
+        #     "created": date.today().strftime("%Y-%m-%d"),
+        #     "username": "test",
+        #     "skills": []
+        # }]
     }
 
     response = client.get("/vacancy/")
